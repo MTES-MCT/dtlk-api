815e5b962ee0104bc596307ec9afec6ae9fcee9a
FERJANI Yassine <yassine.ferjani@soprasteria.com>
2020-01-07 10:55:05 +0100

    Correction du bug au niveau des données retournées

--- src/api/internet.diffusion/middlewares/datafiles.js
+++ src/api/internet.diffusion/middlewares/datafiles.js
@@ -57,11 +57,11 @@ module.exports = {
           return accumulatorMillesimes
         }, [])
       
-      res.locals.datafileMillesime = res.locals.datafileMillesime ? moment(res.locals.datafileMillesime).format('YYYY-MM') : listMillesime[mongoDatafile.extras.datalake_millesimes - 1]
+      res.locals.datafileMillesime = res.locals.datafileMillesime ? moment(res.locals.datafileMillesime).format('YYYY-MM') : listMillesime[mongoDatafile.extras.datalake_millesimes - 1].format('YYYY-MM')
       if (!listMillesime.includes(res.locals.datafileMillesime)) return next(new apiErrors.NotFoundError(`Le millésime ${ res.locals.datafileMillesime } n'existe pas pour le fichier de données avec le rid ${ res.locals.datafileRid }`))
       res.locals.datafileMillesimed = await transformForApi.mongo.datafileMillesime(mongoDatafile, res.locals.datafileMillesime)
       res.locals.datafileMillesimed.previous_millesime_href = ((listMillesime.indexOf(res.locals.datafileMillesime) + 1) > 1) ? `${ apiPublicUrl }v1/datafiles/${ res.locals.datafileRid }?millesime=${ listMillesime[listMillesime.indexOf(res.locals.datafileMillesime) - 1] }` : null
-      res.locals.datafileMillesimed.next_millesime_href = ((listMillesime.indexOf(res.locals.datafileMillesime) + 1) < mongoDatafile.extras.datalake_millesimes) ? `${ apiPublicUrl }v1/datafiles/${ res.locals.datafileRid }?millesime=${ listMillesime[listMillesime.indexOf(res.locals.datafileMillesime) + 1] }` : null
+      res.locals.datafileMillesimed.next_millesime_href = ((listMillesime.indexOf(res.locals.datafileMillesime) + 1) < listMillesime.lenght) ? `${ apiPublicUrl }v1/datafiles/${ res.locals.datafileRid }?millesime=${ listMillesime[listMillesime.indexOf(res.locals.datafileMillesime) + 1] }` : null
       return next()
     } catch (error) {
       if (error.constructor.name === 'MongoNotFoundError') return next(new apiErrors.NotFoundError(`Pas de fichier de données avec le rid ${ res.locals.datafileRid }`))
